97b7f1d8b9d865e2712f55429ab92045f5f7ddae
Nav31 <nsingh3104@gmail.com>
2016-04-20 15:18:47 -0400

    User access control complete

--- server/app/routes/users/index.js
+++ server/app/routes/users/index.js
@@ -4,9 +4,13 @@ var mongoose = require('mongoose');
 var User = mongoose.model('User');
 
 router.get('/', (req, res, next) => {
-	User.find({})
-	.then(users => res.json(users))
-	.then(null, next);
+	// if(!req.user) res.sendStatus(401);
+	// else if(!req.user.isAdmin) res.sendStatus(403)
+	// else {
+		User.find({})
+		.then(users => res.json(users))
+		.then(null, next);
+	// }
 });
 
 router.post('/', (req, res, next) => {
@@ -25,20 +29,26 @@ router.param('id', function(req, res, next, id){
 });
 
 router.get("/:id", (req, res, next) => {
-	res.json(req.loggedIn);
+	if(req.user.isAdmin || req.user.equals(req.loggedIn)) {
+		res.json(req.loggedIn);
+	} else {res.sendStatus(401)}
 });
 
 router.put('/:id', (req, res, next) => {
-	User.update(req.loggedIn, req.body, 
-		{new: true, runValidators: true})
-	.then((user) => res.json(user))
-	.then(null, next);
+	if(req.user.isAdmin || req.user.equals(req.loggedIn)) {
+		User.update(req.loggedIn, req.body, 
+			{new: true, runValidators: true})
+		.then((user) => res.json(user))
+		.then(null, next);
+	} else {res.sendStatus(401)}
 });
 
 router.delete('/:id', (req, res, next) => {
-	User.remove(req.loggedIn)
-	.then(() => res.sendStatus(204))
-	.then(null, next);
+	if(req.user.isAdmin) {
+		User.remove(req.loggedIn)
+		.then(() => res.sendStatus(204))
+		.then(null, next);
+	} else {res.sendStatus(401)}
 });
 
 
