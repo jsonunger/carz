232cdeaae6dded8ecb8ed7f8bc4cf88ad0b7bb21
iMoe037 <i.mohamed037@gmail.com>
2016-04-21 18:48:09 -0400

    Home state no CSS

--- browser/js/home/home.js
+++ browser/js/home/home.js
@@ -1,6 +1,30 @@
 app.config(function ($stateProvider) {
     $stateProvider.state('home', {
         url: '/',
-        templateUrl: 'js/home/home.html'
+        templateUrl: 'js/home/home.html',
+        controller: 'homeCtrl'
     });
+});
+
+
+app.controller('homeCtrl', function($scope){
+	$scope.images = ['http://fillmurray.com/1280/350', 'http://placehold.it/1280x350'];
+	$scope.adminReviews = [{
+		name: 'Immad',
+		image: "http://fillmurray.com/300/300"
+		},
+		{
+		name: 'Massimo',
+		image: "http://fillmurray.com/300/300"
+		},
+		{
+		name: 'Navjot',
+		image: "http://fillmurray.com/300/300"
+		},
+		{
+		name: 'Jason',
+		image: "http://fillmurray.com/300/300"
+		}
+	];
+	
 });
