7ab6a77adeda604674c0269fd1bfcd356fdbdff0
Jason Unger <jasonscottunger@gmail.com>
2016-04-22 14:00:49 -0400

    Browse view progress

--- browser/js/browse/browse.controller.js
+++ browser/js/browse/browse.controller.js
@@ -1,14 +1,8 @@
-app.controller('BrowseCtrl', function ($scope, cars) {
+app.controller('BrowseCtrl', function ($scope, cars, makes, years, types) {
    $scope.cars = cars;
-   $scope.makes = [];
-   $scope.years = [];
-   $scope.types = ['Car', 'Truck', 'SUV', 'Van', 'Minivan'];
-   $scope.cars.forEach(function (car) {
-      if ($scope.makes.indexOf(car.make) === -1) $scope.makes.push(car.make);
-      if ($scope.years.indexOf(car.year) === -1) $scope.years.push(car.year);
-   });
-   $scope.makes.sort();
-   $scope.years.sort();
+   $scope.makes = makes;
+   $scope.years = years;
+   $scope.types = types;
    $scope.makeCollapse = true;
    $scope.yearCollapse = true;
    $scope.typeCollapse = true;
--- browser/js/browse/browse.state.js
+++ browser/js/browse/browse.state.js
@@ -6,6 +6,33 @@ app.config(function ($stateProvider) {
       resolve: {
          cars: function (CarFactory) {
             return CarFactory.getCars();
+         },
+         makes: function (cars) {
+            var makes = [];
+            cars.forEach(function (car) {
+               if (makes.indexOf(car.make) === -1) makes.push(car.make);
+            });
+            return makes.sort().map(function (make) {
+               return {name: make};
+            });
+         },
+         years: function (cars) {
+            var years = [];
+            cars.forEach(function (car) {
+               if (years.indexOf(car.year) === -1) years.push(car.year);
+            });
+            return years.sort().map(function (year) {
+               return {name: year};
+            });
+         },
+         types: function (cars) {
+            var types = [];
+            cars.forEach(function (car) {
+               if (types.indexOf(car.type) === -1) types.push(car.type);
+            });
+            return types.sort().map(function (type) {
+               return {name: type};
+            });
          }
       }
    });
