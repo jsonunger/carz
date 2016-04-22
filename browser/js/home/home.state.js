04a6091e8e3ce854061e7cbd361f289e378b5562
iMoe037 <i.mohamed037@gmail.com>
2016-04-22 11:38:14 -0400

    added controller

--- browser/js/home/home.state.js
+++ browser/js/home/home.state.js
@@ -5,26 +5,3 @@ app.config(function ($stateProvider) {
         controller: 'homeCtrl'
     });
 });
-
-
-app.controller('homeCtrl', function($scope){
-	$scope.images = ['http://fillmurray.com/1280/350', 'http://placehold.it/1280x350'];
-	$scope.adminReviews = [{
-		name: 'Immad',
-		image: "http://fillmurray.com/140/140"
-		},
-		{
-		name: 'Massimo',
-		image: "http://fillmurray.com/140/140"
-		},
-		{
-		name: 'Navjot',
-		image: "http://fillmurray.com/140/140"
-		},
-		{
-		name: 'Jason',
-		image: "http://fillmurray.com/140/140"
-		}
-	];
-	
-});
