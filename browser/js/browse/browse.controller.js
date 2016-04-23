1159a34da830a3dde3aaae03f97777719141044f
Nav31 <nsingh3104@gmail.com>
2016-04-23 12:09:10 -0400

    working on filters for browse

--- browser/js/browse/browse.controller.js
+++ browser/js/browse/browse.controller.js
@@ -1,4 +1,4 @@
-app.controller('BrowseCtrl', function ($scope, cars, makes, years, types) {
+app.controller('BrowseCtrl', function ($scope, cars, makes, years, types, CarFactory) {
    $scope.cars = cars;
    $scope.makes = makes;
    $scope.years = years;
@@ -6,4 +6,40 @@ app.controller('BrowseCtrl', function ($scope, cars, makes, years, types) {
    $scope.makeCollapse = true;
    $scope.yearCollapse = true;
    $scope.typeCollapse = true;
+   $scope.filter = () => {
+
+	   	let checkedMakes = $scope.makes.filter((make)=>{
+            // if(!make.checked) return; 
+	   		return make.checked.name;
+	   	})
+	   	let checkedYears = $scope.years.filter((year)=>{
+            // if(!year.checked) return; 
+	   		return year.checked.name;
+	   	})
+	   	let checkedTypes = $scope.types.filter((type)=>{
+            // if(!type.checked) return; 
+	   		return type.checked.name;
+	   	})
+
+         if(checkedMakes.length === 0){
+            checkedMakes = $scope.makes;
+         }
+
+         checkedYears = checkedYears.length ? checkedYears : $scope.years;
+         checkedTypes = checkedTypes.length ? checkedTypes : $scope.types;
+
+
+         var queryObj = {
+            make: { $in: checkedMakes || $scope.makes },
+            year: { $in: checkedYears || $scope.years },
+            type: { $in: checkedTypes || $scope.types }
+         }
+
+         CarFactory.getCars(queryObj)
+         .then(function(cars){
+            $scope.cars = cars;
+         });
+
+   };
+
 });
---
+++ browser/js/user/user.controller.js
@@ -0,0 +1,5 @@
+
+
+app.controller('UserCtrl', function($scope) {
+	
+})
