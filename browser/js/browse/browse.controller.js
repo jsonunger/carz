b7dfaa6fa99dd8026864f9d049e78c61bd963459
Massimo Crapanzano <massimo.crapanzano@gmail.com>
2016-04-23 14:31:34 -0400

    filter function working

--- browser/js/browse/browse.controller.js
+++ browser/js/browse/browse.controller.js
@@ -8,37 +8,38 @@ app.controller('BrowseCtrl', function ($scope, cars, makes, years, types, CarFac
    $scope.typeCollapse = true;
    $scope.filter = () => {
 
-	   	let checkedMakes = $scope.makes.filter((make)=>{
-            // if(!make.checked) return; 
-	   		return make.checked.name;
-	   	})
-	   	let checkedYears = $scope.years.filter((year)=>{
-            // if(!year.checked) return; 
-	   		return year.checked.name;
-	   	})
-	   	let checkedTypes = $scope.types.filter((type)=>{
-            // if(!type.checked) return; 
-	   		return type.checked.name;
-	   	})
+      $scope.cars = cars;
+      var filtered = [];
 
-         if(checkedMakes.length === 0){
-            checkedMakes = $scope.makes;
-         }
-
-         checkedYears = checkedYears.length ? checkedYears : $scope.years;
-         checkedTypes = checkedTypes.length ? checkedTypes : $scope.types;
-
-
-         var queryObj = {
-            make: { $in: checkedMakes || $scope.makes },
-            year: { $in: checkedYears || $scope.years },
-            type: { $in: checkedTypes || $scope.types }
-         }
-
-         CarFactory.getCars(queryObj)
-         .then(function(cars){
-            $scope.cars = cars;
+      function checkIfChecked(array){
+         var checked = false;
+         array.forEach((obj)=> {
+            if(obj.checked) checked = true;
          });
+         return checked;
+      }
+
+      function addToFiltered(arrayToAdd) {
+         if(!checkIfChecked(arrayToAdd)) {
+            arrayToAdd.forEach((property) =>  filtered.push(property.name));
+         } else {
+            arrayToAdd.forEach((property) => {
+               if(property.checked){
+                  filtered.push(property.name);
+               }
+            });
+         }
+      };
+
+      addToFiltered($scope.makes);
+      addToFiltered($scope.years);
+      addToFiltered($scope.types);
+
+      $scope.cars = $scope.cars.filter((car)=>{
+         return (filtered.includes(car.make) &&
+            filtered.includes(car.year) &&
+            filtered.includes(car.type));
+      });
 
    };
 
