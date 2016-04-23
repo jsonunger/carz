006d08e138a70307b0cc879eb1299ca0c8edca25
Massimo Crapanzano <massimo.crapanzano@gmail.com>
2016-04-23 15:08:58 -0400

    some change but working

--- browser/js/car/car.controller.js
+++ browser/js/car/car.controller.js
@@ -1,4 +1,4 @@
-app.controller('CarCtrl', function($scope, CarFactory, currentCar, AuthService, user){
+app.controller('CarCtrl', function($scope, CarFactory, currentCar, user){
     $scope.mainImage = 'http://fillmurray.com/500/400';
     $scope.title = currentCar.year + " " + currentCar.make + " " + currentCar.model;
     $scope.car = currentCar;
