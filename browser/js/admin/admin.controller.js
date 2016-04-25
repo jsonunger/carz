2939b3d53045dd17cd7eaca07e4bab9bbedf88ea
Nav31 <nsingh3104@gmail.com>
2016-04-24 20:01:55 -0400

    Admin Product tab, things show, need to fix user OBJ

--- browser/js/admin/admin.controller.js
+++ browser/js/admin/admin.controller.js
@@ -1,4 +1,15 @@
 app.controller('AdminCtrl', ($scope, users, cars) => {
 	$scope.users = users;
-	$scope.cars = cars;
+	$scope.cars = cars.map((car) => {
+		return [
+			{ label: 'make', value: car.make },
+			{ label: 'model', value: car.model },
+			{ label: 'photo', value: car.photo },
+			{ label: 'price', value: car.price },
+			{ label: 'quantity', value: car.quantity },
+			{ label: 'type', value: car.type },
+			{ label: 'year', value: car.year }
+		]
+	});
+	console.log(cars);
 })
