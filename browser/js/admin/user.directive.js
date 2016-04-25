d7a78c3a585c35e9169f759289177bcceabc47c1
Massimo Crapanzano <massimo.crapanzano@gmail.com>
2016-04-25 15:28:30 -0400

    fixed admin tools page

--- browser/js/admin/user.directive.js
+++ browser/js/admin/user.directive.js
@@ -1,26 +1,31 @@
 
 
-app.directive('user', (OrderFactory)=> {
+app.directive('user', (OrderFactory, $log)=> {
 	return {
 		restrict: 'E',
 		templateUrl: '/js/admin/user.directive.html',
 		scope:{
-			users: "="
+			user: "="
 		},
 		link: (scope, element) => {
 			scope.getOrders = (id) => {
+                if(scope.cart) {
+                    scope.cart = null;
+                    return;
+                }
 				OrderFactory.getCart(id)
 				.then((order) => {
 					scope.cart = order.map((cart) => {
 						return [
-						{label: "cars", value: cart.cars},
-						{label: "shipping address", value: cart.shipping},
-						{label: "billing address", value: cart.billing},
-						{label: "complete", value: cart.completed},
-						{label: "price", value: cart.price}
-					]
-				});
-				});
+    						{label: "cars", value: cart.cars},
+    						{label: "shipping address", value: cart.shipping},
+    						{label: "billing address", value: cart.billing},
+    						{label: "complete", value: cart.completed},
+    						{label: "price", value: cart.price}
+    					];
+    				});
+				})
+                .catch($log.error);
 			}
 		}
 	}
