fe7c4e7aae939b74716d237546343347c645af35
Jason Unger <jasonscottunger@gmail.com>
2016-04-25 12:22:02 -0400

    Review Directive

--- browser/js/car/car.controller.js
+++ browser/js/car/car.controller.js
@@ -1,6 +1,13 @@
 app.controller('CarCtrl', function($scope, CarFactory, currentCar, user, reviews){
-    $scope.mainImage = 'http://fillmurray.com/500/400';
-    $scope.title = currentCar.year + " " + currentCar.make + " " + currentCar.model;
-    $scope.car = currentCar;
-    $scope.reviews = reviews;
+   $scope.mainImage = 'http://fillmurray.com/500/400';
+   $scope.title = currentCar.year + " " + currentCar.make + " " + currentCar.model;
+   $scope.car = currentCar;
+   $scope.reviews = reviews;
+   $scope.showRev = false;
+   $scope.rating = function (num) {
+      return new Array(num);
+   };
+   $scope.showReviews = function () {
+      $scope.showRev = !$scope.showRev;
+   };
 });
