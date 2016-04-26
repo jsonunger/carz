fe44b48d825b30053780bb8613dbe5f03364bed9
Nav31 <nsingh3104@gmail.com>
2016-04-26 14:17:01 -0400

    removed CL

--- server/app/routes/stripe/index.js
+++ server/app/routes/stripe/index.js
@@ -5,7 +5,6 @@ var stripe = require('stripe')("sk_test_ykXIr4Utc2pigKQm0GE5Qn7Q");
 router.post('/', (req, res, next) => {
 	console.log("IM A REQ",req.body)
 	stripe.charges.create(req.body, function(err, charge) {
- 		console.log(charge);
 	});
 })
 
