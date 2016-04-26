6703c78ff5af5dc11d4ca6587364f1db3eb9853c
iMoe037 <i.mohamed037@gmail.com>
2016-04-26 10:45:39 -0400

    added seed info

--- browser/js/car/car.controller.js
+++ browser/js/car/car.controller.js
@@ -1,5 +1,4 @@
 app.controller('CarCtrl', function($scope, CarFactory, currentCar, user, reviews, OrderFactory, $rootScope, $state){
-   $scope.mainImage = 'http://fillmurray.com/500/400';
    $scope.title = currentCar.year + " " + currentCar.make + " " + currentCar.model;
    $scope.car = currentCar;
    $scope.reviews = reviews;
