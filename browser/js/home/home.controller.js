a3287b896ea025981327f8d75d6b972d95ec6616
Nav31 <nsingh3104@gmail.com>
2016-04-27 10:51:33 -0400

    what i have

--- browser/js/home/home.controller.js
+++ browser/js/home/home.controller.js
@@ -1,25 +1,30 @@
 app.controller('homeCtrl', function($scope, carousel){
 	$scope.carousel = carousel;
 	$scope.images = ['http://fillmurray.com/1280/350', 'http://placehold.it/1280x350'];
+	console.log(carousel)
 	$scope.adminReviews = [{
 		name: 'Immad',
 		image: carousel[0].photo[0],
-		description: carousel[0].description
+		description: carousel[0].description,
+		id: carousel[0]._id
 		},
 		{
 		name: 'Massimo',
 		image: carousel[1].photo[0],
-		description: carousel[1].description
+		description: carousel[1].description,
+		id: carousel[1]._id
 		},
 		{
 		name: 'Navjot',
 		image: carousel[2].photo[0],
-		description: carousel[2].description
+		description: carousel[2].description,
+		id: carousel[2]._id
 		},
 		{
 		name: 'Jason',
 		image: carousel[3].photo[0],
-		description: carousel[3].description
+		description: carousel[3].description,
+		id: carousel[3]._id
 		}
 	];
 	
